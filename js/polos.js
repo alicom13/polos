b242bccdde033c7102c1f66a0b0924402b2c11b3
Ali Musthofa <alfaonline13@gmail.com>
2025-05-13 10:44:37 +0700

    Create polos.js

---
+++ js/polos.js
@@ -0,0 +1,18 @@
+/*!
+ * Polos v0.0.1 (https://wsw.biz.id/)
+ * Copyright 2025 Polos Style
+ * Unlicensed
+ */
+
+/* Mulai $( document ).ready()
+$(function() {
+    console.log( "polos ready!" );
+});
+*/
+// Jika window kurang dari 576px
+if($(window).width() < 576px)
+{
+  // Fungsi untuk layar kecil
+} else {
+  // Funsi untuk layar lebar
+}
