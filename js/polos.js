4ddb6391dabfdb6d86082f9249790d555c2403e0
Ali Musthofa <alfaonline13@gmail.com>
2025-05-13 13:09:06 +0700

    Update polos.js

--- js/polos.js
+++ js/polos.js
@@ -7,9 +7,14 @@
  * Polos Jquery
  */
 $(document).ready(function(){
-    colsole.log( "Polos Style" );
-    if( $(window).width() < 576px ){
+    // colsole.log( "Polos Style" );
+    if( window.matchMedia('(max-width: 575.98px)').matches){
         // Fungsi Layar Sempit
+        $( "div.rw.m-2.p-3" ).removeClass( "m-2 p-3" );
+        // ganti css
+        $( "div.lmn.shadow-sm.m-2.p-3" ).removeClass( "p-3" ).addClass( "p-1" );
+        // atribut baru
+        $("div.lmn.shadow-sm.m-2.p-3").css({"padding": "0 !important", "font-size": "90%","border":"1px solid gold"});
     } else {
         // Fungsi Layar Lebar
     }
